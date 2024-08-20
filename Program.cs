26435cea9549428aa2645ac2b07039abf3f7ff41
Андрей Викулов <7432721@gmail.com>
2024-08-20 14:20:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Как Вас зовут: ");
+        var name = Console.ReadLine();
+        Console.Write("Укажите Ваш возраст: ");
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.Write("Укажите Вашу дату рождения в формате дд.мм.гггг: ");
+        var date = Console.ReadLine();
+
+//        Console.Write("What is your favorite day of week? ");
+//        day_of_week dt = (day_of_week)int.Parse(Console.ReadLine());
+//        Console.Write("Your favorite day of week is {0}", dt);
+        
+        Console.WriteLine("Вас зовут {0} а Вам {1} лет", name, age);
+        Console.WriteLine("Ва родились {0}", date);
+        Console.ReadKey();
+    }
+}
+
+enum day_of_week : byte
+{ понедельник = 1, вторник, среда, четверг, пятница, суббота, воскресенье }
